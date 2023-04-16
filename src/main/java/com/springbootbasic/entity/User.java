86a783e5fde39d778c1903c7ba64263e62168d2e
Praveen <praveenreddyshetty@gmail.com>
2023-04-17 03:15:59 +0530

    "Basic"

---
+++ src/main/java/com/springbootbasic/entity/User.java
@@ -0,0 +1,11 @@
+package com.springbootbasic.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+public class User {
+    private String id;
+    private String name;
+    private String email;
+}
